bef4cca2b10eaa16353ad472ce5332ba1e8ee8bf
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2022-03-01 10:49:23 +0000

    fix breaking test

--- internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource_test.go
+++ internal/services/maintenance/maintenance_assignment_virtual_machine_scale_set_resource_test.go
@@ -115,7 +115,6 @@ resource "azurerm_lb" "test" {
 
 resource "azurerm_lb_backend_address_pool" "test" {
   name                = "test"
-  resource_group_name = azurerm_resource_group.test.name
   loadbalancer_id     = azurerm_lb.test.id
 }
 
